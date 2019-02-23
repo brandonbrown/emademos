13d44e17509521efd935f84d8ac5fff18e2f7ba9
brandonbrown <brandon@bybrandonbrown.com>
2019-02-22 23:12:34 -0500

    Updates

---
+++ precache-manifest.e7e1018990c180ba1e69a45af46b8fdc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/emademos/client/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7b39573ad400feb80698d7b353bf6f9d",
+    "url": "/emademos/client/static/media/band1.7b39573a.png"
+  },
+  {
+    "revision": "c54e38cc76a0ef2a970c",
+    "url": "/emademos/client/static/js/runtime~main.c54e38cc.js"
+  },
+  {
+    "revision": "13d8a8da655038a44326",
+    "url": "/emademos/client/static/js/main.13d8a8da.chunk.js"
+  },
+  {
+    "revision": "a1a1f634a75bdcb909f9",
+    "url": "/emademos/client/static/js/2.a1a1f634.chunk.js"
+  },
+  {
+    "revision": "13d8a8da655038a44326",
+    "url": "/emademos/client/static/css/main.8b47707e.chunk.css"
+  },
+  {
+    "revision": "82fcdb9a4db89678b6db2ee32f5da47d",
+    "url": "/emademos/client/index.html"
+  }
+];
